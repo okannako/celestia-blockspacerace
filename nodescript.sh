d0b5a222a792e995943e8bf2a63d28d91b5ac4f8
NakoTurk <filmoloji89@gmail.com>
2023-05-14 23:12:34 +0300

    Update nodescript.sh

--- nodescript.sh
+++ nodescript.sh
@@ -14,22 +14,7 @@ echo -e '\e[36mGithub  :\e[39m' https://github.com/okannako
 echo -e '\e[36mYoutube :\e[39m' https://www.youtube.com/@CryptoChainNakoTurk
 echo -e "\e[0m"
 sleep 5
-echo -e "\e[1m\e[32m Updates \e[0m" && sleep 2
-sudo apt update && sudo apt upgrade -y
-sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git ncdu -y
-sudo apt install make -y && cd $HOME
-sleep 1
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo -e "\e[1m\e[32m Install Go \e[0m" && sleep 2
-ver="1.20.3"
-cd $HOME
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-rm "go$ver.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
-source $HOME/.bash_profile
-go version && sleep 2
+
 echo -e "\e[1m\e[32m What is it you want to do? \e[0m" && sleep 2
 PS3='Select an action: '
 options=(
