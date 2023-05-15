b87da54e3a050616cc001b94c0c38bda3ff2d259
NakoTurk <filmoloji89@gmail.com>
2023-05-15 17:05:56 +0300

    Update nodescript.sh

--- nodescript.sh
+++ nodescript.sh
@@ -38,49 +38,34 @@ case $opt in
 "Validator Node Install")
 
 echo -e "\e[1m\e[32m Updates \e[0m" && sleep 2
-sudo apt update && sudo apt upgrade -y
-sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git ncdu -y
-sudo apt install make -y && cd $HOME
-sleep 1
-
-echo -e "\e[1m\e[32m Install Go \e[0m" && sleep 2
-ver="1.20.3"
-cd $HOME
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-rm "go$ver.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
-source $HOME/.bash_profile
-go version && sleep 2
-
 cd $HOME 
 rm -rf celestia-app 
 git clone https://github.com/celestiaorg/celestia-app.git 
 cd celestia-app/ 
-APP_VERSION=v0.13.0
-git checkout tags/$APP_VERSION -b $APP_VERSION 
+APP_VERSION=v0.13.0 
+git checkout tags/$APP_VERSION -b $APP_VERSION
 make install
-celestia-appd && sleep 3
+celestia-appd version && sleep 3
+
+cd $HOME
+rm -rf networks
+git clone https://github.com/celestiaorg/networks.git
 
 echo "NodeName:"
-read NODENAME
-echo export NODENAME=${NodeName} >> $HOME/.bash_profile
+read NodeName
+echo export NodeName=${NodeName} >> $HOME/.bash_profile
 
 celestia-appd init "$NodeName" --chain-id blockspacerace-0
 
 cp $HOME/networks/blockspacerace/genesis.json $HOME/.celestia-app/config
 
-curl -Ls https://snapshots.kjnodes.com/celestia-testnet/addrbook.json > $HOME/.celestia-app/config/addrbook.json
+peers="b766d36a1e3bcefc5e5befddfad7b4589ba28a21@162.55.242.83:26656,c97019ef9ee43e93ad9019514b612e6b8363c3fd@138.201.63.38:26686,62f6abc162db99389f13a1cdf1abaeb6efb647a7@35.210.78.75:26656,6c73374cb78a543e2dd3eb218c29386392da2cf5@35.210.99.77:26656,5fa6853eb52bc3a5ff1fe56b988515d16644819a@65.21.232.33:2000,de36dc2bc32ecaacafb213d173f6218f93ebb306@144.76.105.14:26656,ae95e8d93a0822a763823551c163d15d4cdce944@116.202.227.117:20656,af66f28f19f747bd2b5a18d91d143dc8e035f86a@47.147.226.228:52656,d5519e378247dfb61dfe90652d1fe3e2b3005a5b@65.109.68.190:12056,0196b56324c6fd3dd31110d3cb06dc169a1e1310@194.62.97.31:26656"
+sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$peers\"|" $HOME/.celestia-app/config/config.toml
 
 PRUNING="nothing"
 sed -i -e "s/^pruning *=.*/pruning = \"$PRUNING\"/" $HOME/.celestia-app/config/app.toml
 
 celestia-appd tendermint unsafe-reset-all --home $HOME/.celestia-app
-
-echo -e '\e[1m\e[32m Downloading Snapshot for fast synchronisation to the current block. \e[0m' && sleep 2
-
-curl -L https://snapshots.kjnodes.com/celestia-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.celestia-app
 
 sudo tee <<EOF >/dev/null /etc/systemd/system/celestia-appd.service
 [Unit]
