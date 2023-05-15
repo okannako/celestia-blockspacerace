3ef7ff2f014e28ed9bb5473b5a7c6113d993ace9
NakoTurk <filmoloji89@gmail.com>
2023-05-15 17:25:39 +0300

    Update nodescript.sh

--- nodescript.sh
+++ nodescript.sh
@@ -38,6 +38,23 @@ case $opt in
 "Validator Node Install")
 
 echo -e "\e[1m\e[32m Updates \e[0m" && sleep 2
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git ncdu -y
+sudo apt install make -y && cd $HOME
+sleep 1
+
+echo -e "\e[1m\e[32m Install Go \e[0m" && sleep 2
+ver="1.20.3"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+go version && sleep 2
+
 cd $HOME 
 rm -rf celestia-app 
 git clone https://github.com/celestiaorg/celestia-app.git 
